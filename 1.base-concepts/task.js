d1945e9088ba3cb86e69ecabf165bccf91234dc4
Igor Smirnov <inextgen@mail.ru>
2023-07-11 22:43:37 +0300

    task.js updated

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -24,16 +24,21 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
+	//Суть проверки в том, что если какое-то из значений функции нельзя привести в числовому типу вернет false.
 	if (Number.isNaN(Number(percent)) || Number.isNaN(Number(contribution)) || Number.isNaN(Number(amount)) || Number.isNaN(Number(countMonths))) {
 		return false;
 	}
 
+	//Также значения функции по логике задачи должны быть больше или равны 0
 	if (percent >= 0 && contribution >= 0 && amount >= 0 && countMonths >= 0) {
+		percent = Number(percent);
+		contribution = Number(contribution);
+		amount = Number(amount);
+		countMonths = Number(countMonths);
 		let percent01 = percent / 100 / 12;
 		let payment = (amount - contribution) * (percent01 + (percent01 / (((1 + percent01) ** countMonths) - 1)));
 		return Number((payment * countMonths).toFixed(2));
 	} else
 		console.log("Значения функции должны быть больше или равны нулю!")
 	return false;
-
 }
